d268a8098cc524342bff8af4165123d8cb35bfc4
smejiatest <smejiatest@gmail.com>
2024-02-02 17:16:48 -0500

    testng version and javaBasics classes created

---
+++ src/main/java/javaBasics/HolaMundo.java
@@ -0,0 +1,21 @@
+package javaBasics;
+
+
+
+import com.google.common.annotations.VisibleForTesting;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+    } // main ends
+
+
+
+
+
+
+
+} // Claas ends
---
+++ src/test/java/javaBasics/HolaMundo.java
@@ -0,0 +1,17 @@
+package javaBasics;
+
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+    } // main ends
+
+
+
+
+
+
+
+} // Claas ends
