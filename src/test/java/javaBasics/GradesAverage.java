0324368d30ffb54507e5534b7c0b429cf2703a61
smejiatest <smejiatest@gmail.com>
2024-02-06 15:23:34 -0500

    Do while exercise

--- src/test/java/javaBasics/GradesAverage.java
+++ src/test/java/javaBasics/GradesAverage.java
@@ -1,7 +1,6 @@
 package javaBasics;
 
 import org.testng.Reporter;
-import org.testng.annotations.Test;
 
 import java.util.Scanner;
 
@@ -26,12 +25,55 @@ public class GradesAverage {
             grade2 = reader.nextDouble();
         } while (grade1 < 0 || grade1 > 100 || grade2 < 0 || grade2 > 100);
 
-        Reporter.log("Grades average is: " + (grade1 + grade2) / 2, true);
+        double average = averageResult(grade1,grade2);
+        Reporter.log("Grades average is: " + average, true);
+
+        averageCalculation();
+        double averageReturned = averageCalculationReturn(87);
+        Reporter.log("Average returned is: " + averageReturned, true);
 
 
     }
 
+    public static double averageCalculationReturn(double average) {
 
+        double average2 = ((grade1 + grade2) / 2);
+        Reporter.log("The average is: " + average2, true);
+
+        if (average2 > 70 && average2 <= 100) {
+            Reporter.log("You passed with " + average2, true);
+
+        } else if (average2 <= 70 && average2 >= 0) {
+            Reporter.log("You didn't pass with " + average2, true);
+
+        }
+
+        return average;
+
+    }
+
+
+    public static void averageCalculation() {
+
+        double average = ((grade1 + grade2) / 2);
+        Reporter.log("The average is: " + average, true);
+
+        if (average > 70 && average <= 100) {
+            Reporter.log("You passed with " + average, true);
+
+        } else if (average <= 70 && average >= 0) {
+            Reporter.log("You didn't pass with " + average, true);
+
+        } // End averageCalculation
+
+    }
+
+
+    public static double averageResult (double num1, double num2 ) {
+        return (num1+num2)/2;
+    }
+
+    // End averageCalculation
 //    @Test
 //
 //    public void averageCalculation() {
@@ -53,17 +95,22 @@ public class GradesAverage {
 //
 //    } // End averageCalculation
 
-    public void keyboardAverageCalculation() {
+//    public void keyboardAverageCalculation() {
+//
+//        Scanner reader = new Scanner(System.in);
+//        Reporter.log("Type grade 1", true);
+//        grade1 = reader.nextDouble();
+//
+//        Reporter.log("Type grade 2", true);
+//        grade2 = reader.nextDouble();
+//
+//        Reporter.log("Grades average is: " + (grade1 + grade2) / 2, true);
+//
+//
+//    } // End keyboardAverageCalculation
 
-        Scanner reader = new Scanner(System.in);
-        Reporter.log("Type grade 1", true);
-        grade1 = reader.nextDouble();
 
-        Reporter.log("Type grade 2", true);
-        grade2 = reader.nextDouble();
-
-        Reporter.log("Grades average is: " + (grade1 + grade2) / 2, true);
+} // End averageCalculation
 
 
-    } // End keyboardAverageCalculation
-}
+
